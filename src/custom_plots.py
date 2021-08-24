5a875f669b87a453aaf8d02764eef09ce25dc5c1
Seth Schober <seth.schober@gmail.com>
2021-08-23 22:53:00 -0800

    Separating functionality into distinct functions reusable across multiple notebooks. Cleaning code. Adding to Final notebook

--- src/custom_plots.py
+++ src/custom_plots.py
@@ -2,7 +2,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import shap
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from imblearn.over_sampling import SMOTE
 
 def custom_plot_matrix(df, max_corr=None):
     ## Compute the correlation matrix
@@ -24,3 +26,69 @@ def custom_plot_matrix(df, max_corr=None):
     sns.heatmap(corr, mask=mask, cmap=cmap, center=0, vmax=max_corr, vmin=-max_corr, 
                 square=True, linewidths=1, cbar_kws={"shrink": .4})
     plt.show()
+
+
+
+
+# ADAPTED FROM CODE FOUND HERE: https://towardsdatascience.com/explain-any-models-with-the-shap-values-use-the-kernelexplainer-79de9464897a
+def get_shap_df(df_train, target_train, df_test):
+    
+    df_train.reset_index(inplace=True, drop=True)
+    df_test.reset_index(inplace=True, drop=True)
+    
+    # CATEGORICALS
+    df_train_cat = df_train.select_dtypes('object')
+    df_test_cat = df_test.select_dtypes('object')
+    ohe = OneHotEncoder(sparse=False, handle_unknown='ignore')
+    ohe.fit(df_train_cat)
+    df_train_cat_ohe = ohe.transform(df_train_cat)
+    df_test_cat_ohe = ohe.transform(df_test_cat)
+    names_ohe = ohe.get_feature_names(df_train_cat[df_train_cat.columns].columns)
+    
+    # NUMERIC
+    df_train_num = df_train.select_dtypes('number')
+    df_test_num = df_test.select_dtypes('number')
+    
+    # COMBINE CATEGORICAL AND NUMERIC
+    df_train_expanded = pd.DataFrame(df_train_cat_ohe, columns=names_ohe)
+    df_test_expanded = pd.DataFrame(df_test_cat_ohe, columns=names_ohe)
+    
+    df_train_expanded[df_train_num.columns] = df_train_num
+    df_test_expanded[df_test_num.columns] = df_test_num
+    
+    # SCALE DATA
+    ss = StandardScaler()
+    ss.fit(df_train_expanded)
+    df_train_expanded_scaled = ss.transform(df_train_expanded)
+    df_test_expanded_scaled = ss.transform(df_test_expanded)    
+    
+    # CONVERT FROM ARRAY TO DF
+    df_train_expanded_scaled = pd.DataFrame(df_train_expanded_scaled, columns=df_train_expanded.columns)
+    df_test_expanded_scaled = pd.DataFrame(df_test_expanded_scaled, columns=df_test_expanded.columns)
+    
+    sm = SMOTE()
+    x_train_final, y_train_final = sm.fit_resample(df_train_expanded_scaled, target_train)    
+    
+    return x_train_final, y_train_final, df_test_expanded_scaled
+
+
+def produce_shap_plot(df_train, target_train, df_test, target_test, model_shap):
+    
+    # Gather encoded, scaled, resampled dataframes
+    df_train, target_train, df_test = get_shap_df(df_train, target_train, df_test)
+    
+    # Extract "model" portion of pipeline
+    model_shap = model_shap.steps[2][1]
+    
+    # Fit to training data
+    model_shap.fit(df_train, target_train)
+    pred = model_shap.predict(df_test)
+    
+    # Produce shap values
+    explainer = shap.TreeExplainer(model_shap)
+    shap_values = explainer.shap_values(df_test)
+    
+    # plot findings
+    plt.figure()
+    shap.summary_plot(shap_values, df_test, show=False, plot_size=(16, 12))
+    plt.show();
--- src/remove_missing_data.py
+++ src/remove_missing_data.py
@@ -1,7 +1,9 @@
-import pandas
+import numpy as np
+import pandas as pd
 
-def remove_missing_data(df):
-
+def remove_missing_data(df, MAX_MISSING_VALUES=250):
+    # Max missing values is a semi-arbitrary threshold for the maximum number of missing values to justify keeping
+    
     # Constants used throughout analysis
     NA_STRING = "NA_SS"
     NA_NUMBER = -999.0
@@ -10,9 +12,6 @@ def remove_missing_data(df):
     df.replace(NA_STRING, np.nan, inplace=True)
     df.replace(NA_NUMBER, np.nan, inplace=True)
 
-    # Set semi-arbitrary threshold for the maximum number of missing values to justify keeping
-    MAX_MISSING_VALUES = 250
-
     # Determine the number of missing values in each column
     for col in df.columns:
         na_ct = df[col].isna().sum()
@@ -20,5 +19,5 @@ def remove_missing_data(df):
         if na_ct > MAX_MISSING_VALUES:
             df.drop(col, axis=1, inplace=True)
 
-    df_cut.dropna(inplace=True)
+    df.dropna(inplace=True)
     return df
